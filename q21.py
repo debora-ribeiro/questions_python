07986acddd74abab7a01140dae33b7e5671569e5
Débora Ribeiro <117853740+debora-ribeiro@users.noreply.github.com>
2023-06-14 17:43:11 -0300

    Remuneraçaõ dia com desconto de 8%

---
+++ q21.py
@@ -0,0 +1,4 @@
+remuneracao_dia = int(input ("Quantos dias serão necessarios pararealizar o trabalho? "))
+valor = int( remuneracao_dia * 30)
+remuneracao_final = valor - valor * 8 /100
+print(remuneracao_final)
