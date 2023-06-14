2e72b007793348f814643050ff3b70348af1eba3
Débora Ribeiro <117853740+debora-ribeiro@users.noreply.github.com>
2023-06-14 16:59:24 -0300

    Divisão de valor em porcentagens

---
+++ q20.py
@@ -0,0 +1,8 @@
+valor =  780.000
+#O valor deverá ser dividido entre 3 ganhadores em porcentagens diferentes
+ganhador_1 = 780.000 * 46/100
+print(ganhador_1)
+ganhador_2 = 780.000 * 32/100
+print(ganhador_2)
+ganhador_3 = 780.000 - ganhador_1 - ganhador_2
+print(ganhador_3)
