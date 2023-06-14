68ab6858904079b4d5af1795736262a12cdc0b43
Débora Ribeiro <117853740+debora-ribeiro@users.noreply.github.com>
2023-06-14 09:27:43 -0300

    Média de 4 notas

---
+++ q16.py
@@ -0,0 +1,6 @@
+nota_1 = 8.0
+nota_2 = 10.0
+nota_3 = 5.0
+nota_4 = 9.0
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+print(media)
---
+++ q17.py
@@ -0,0 +1,5 @@
+numero = 12
+antecessor = numero - 1
+sucessor = numero + 1
+print(antecessor)
+print(sucessor)
