ae4674c619d2bc31150b576778a03d961e6d7e07
Débora Ribeiro <117853740+debora-ribeiro@users.noreply.github.com>
2023-06-16 15:42:34 -0300

    Transformar letra maiúscula em minúscula e vice-versa

---
+++ q23.py
@@ -0,0 +1,8 @@
+# converter letra maiuscula emminuscula
+animal ="GATO"
+nome_animal = animal.lower()
+print(nome_animal)
+#converter letra minuscula emmaiuscula
+animal2 = "ovelha"
+nome_animal2 = animal2.upper()
+print(nome_animal2)
